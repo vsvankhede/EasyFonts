d0154015aa118249e4a49ac0215c9add3f87abdd
IsisplAndroidVijay <vijay.vankhede@isispl.com>
2015-06-21 10:17:38 +0530

    project

---
+++ app/src/main/java/com/vstechlab/testeasyfont/testeasyfont/MainActivity.java
@@ -0,0 +1,44 @@
+package com.vstechlab.testeasyfont.testeasyfont;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.vstechlab.easyfonts.EasyFonts;
+
+public class MainActivity extends ActionBarActivity {
+    TextView tv_hello_world;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        tv_hello_world = (TextView) findViewById(R.id.tv_hello_world);
+        tv_hello_world.setTypeface(EasyFonts.recognition(this));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
